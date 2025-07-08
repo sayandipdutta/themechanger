33c5b66ab7cd4e48dd137abfe03e82e2d6098209
Sayandip Dutta <sayandip199309@gmail.com>
2025-07-09 01:42:26 +0530

    Write logs to localappdata

--- main.go
+++ main.go
@@ -13,13 +13,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var themeFlag *bool
 
-// Help message
 func helpMessage() {
 	fmt.Println("\nFlags:")
 	flag.PrintDefaults()
@@ -28,23 +29,44 @@ func helpMessage() {
 	fmt.Println(".\\themeChange.exe -light")
 }
 
+func getLogFolder() (string, bool) {
+	appDataDir := os.Getenv("LOCALAPPDATA")
+	if appDataDir == "" {
+		return "", false
+	}
+	logfolder := filepath.Join(appDataDir, "themechanger")
+	if info, err := os.Stat(logfolder); err != nil || !info.IsDir() {
+		err = os.Mkdir(logfolder, 0666)
+		if err != nil {
+			return "", false
+		}
+	}
+	return logfolder, true
+}
+
 func main() {
 	flag.Usage = helpMessage
-	themeFlag = flag.Bool("light", false, "If given, `light` theme will be set.")
+	themeFlag = flag.Bool("light", false, "If given, `light` theme will be set, otherwise `dark`.")
 	flag.Parse()
 
-	file, err := os.OpenFile("themechanger.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
+	logfolder, ok := getLogFolder()
+	if ok {
+		logfile := filepath.Join(logfolder, "themechanger.log")
+		logwriter, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		defer logwriter.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.SetOutput(io.MultiWriter(os.Stderr, logwriter))
+	} else {
+		log.SetOutput(os.Stderr)
 	}
-	log.SetOutput(file)
 	log.Printf("INFO: Application started successfully!")
 
 	var conf map[string]themeConfig
-	conf, err = loadConfig()
+	conf, err := loadConfig()
 	if err != nil {
-		log.Fatalln("ERROR: Whie loading config ->", err)
+		log.Fatalln("WARNING: Error while loading config ->", err, " Skipping!")
 	}
 
 	theme := "dark"
